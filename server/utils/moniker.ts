a8c3c6869e4d45a75ddaefd82674d2c7cf35829b
Pooh303 <65070182@kmitl.ac.th>
2024-10-19 00:28:09 +0700

    upload

---
+++ server/utils/moniker.ts
@@ -0,0 +1,49 @@
+import fs from 'fs';
+import { resolveShard } from './resolveShard';
+
+let adjectives = fs
+  .readFileSync(process.cwd() + '/words/adjectives.txt')
+  .toString()
+  .split('\n');
+const nouns = fs
+  .readFileSync(process.cwd() + '/words/nouns.txt')
+  .toString()
+  .split('\n');
+const verbs = fs
+  .readFileSync(process.cwd() + '/words/verbs.txt')
+  .toString()
+  .split('\n');
+const randomElement = (array: string[]) =>
+  array[Math.floor(Math.random() * array.length)];
+
+// export function makeRoomName(shard: number | undefined) {
+//   let filteredAdjectives = adjectives;
+//   if (shard) {
+//     // Filter the adjective list by shard
+//     filteredAdjectives = adjectives.filter(
+//       (adj) => resolveShard(adj) === Number(shard),
+//     );
+//   }
+//   const adjective = randomElement(filteredAdjectives);
+//   const noun = randomElement(nouns);
+//   const verb = randomElement(verbs);
+//   return `${adjective}-${noun}-${verb}`;
+// }
+
+export function makeRoomName(shard: number | undefined) {
+  let number = Math.floor(100000 + Math.random() * 900000);
+  if (shard) {
+    number = Math.floor(100000 + Number(shard) * 100000);
+  }
+  return `${number}`;
+}
+
+export function makeUserName() {
+  return `${capFirst(randomElement(adjectives))} ${capFirst(
+    randomElement(nouns),
+  )}`;
+}
+
+function capFirst(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
---
+++ src/components/InviteButton/InviteButton.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Button, Icon, Popup } from 'semantic-ui-react';
+import { InviteModal } from '../Modal/InviteModal';
+
+export const InviteButton = () => {
+  const [inviteModalOpen, setInviteModalOpen] = useState(false);
+
+  return (
+    <>
+      {inviteModalOpen && (
+        <InviteModal closeInviteModal={() => setInviteModalOpen(false)} />
+      )}
+      <Popup
+        content="ชวนเพื่อนมาร่วมตี้!"
+        trigger={
+          <Button
+            color="green"
+            icon
+            labelPosition="left"
+            fluid
+            className="toolButton"
+            style={{ minWidth: '12em',
+              background: 'linear-gradient(to right, #4CAF50, #8BC34A)',
+              fontFamily: 'fonts, sans-serif'
+             }}
+            onClick={() => setInviteModalOpen(true)}
+          >
+            <Icon name="add user" />
+            ชวนเพื่อน</Button>
+        }
+      />
+    </>
+  );
+};
